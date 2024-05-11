a22ad92d9f1ab80e084aa1b2935a4413fc6aaf63
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-05-11 16:18:27 +0700

    refactor: change format result at AuthenticationController

--- src/main/java/com/learning/springsecurity/auth/AuthenticationController.java
+++ src/main/java/com/learning/springsecurity/auth/AuthenticationController.java
@@ -1,15 +1,13 @@
 package com.learning.springsecurity.auth;
 
-
 import com.learning.springsecurity.auth.dto.request.AuthenticationRequest;
 import com.learning.springsecurity.auth.dto.request.LogoutRequest;
 import com.learning.springsecurity.auth.dto.request.RefreshRequest;
 import com.learning.springsecurity.auth.dto.request.RegisterRequest;
+import com.learning.springsecurity.auth.dto.response.APIResponse;
 import com.learning.springsecurity.auth.dto.response.AuthenticationResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,32 +18,40 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public APIResponse<AuthenticationResponse> register(
             @Valid @RequestBody RegisterRequest registerRequest
     ) {
-        return ResponseEntity.ok(authenticationService.register(registerRequest));
+        return APIResponse.<AuthenticationResponse>builder()
+                .result(authenticationService.register(registerRequest))
+                .build();
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public APIResponse<AuthenticationResponse> authenticate(
             @Valid @RequestBody AuthenticationRequest authenticationRequest
     ) {
-        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+        return APIResponse.<AuthenticationResponse>builder()
+                .result(authenticationService.authenticate(authenticationRequest))
+                .build();
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<AuthenticationResponse> refresh(
+    public APIResponse<AuthenticationResponse> refresh(
             @Valid @RequestBody RefreshRequest request
     ) {
-        return ResponseEntity.ok(authenticationService.refresh(request));
+        return APIResponse.<AuthenticationResponse>builder()
+                .result(authenticationService.refresh(request))
+                .build();
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(
+    public APIResponse<String> logout(
             @Valid @RequestBody LogoutRequest request
     ) {
         authenticationService.logout(request);
-        return ResponseEntity.status(HttpStatus.OK).body("Logout successful");
+        return APIResponse.<String>builder()
+                .result("Logout successful")
+                .build();
     }
 
 }
