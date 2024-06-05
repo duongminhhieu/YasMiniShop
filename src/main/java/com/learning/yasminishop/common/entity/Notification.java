60c4abba8dafe91d432e67bcdd6e2be74eb23848
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-06-05 16:07:46 +0700

    chore: change name, artifactId in pom.xml file

--- src/main/java/com/learning/yasminishop/common/entity/Notification.java
+++ src/main/java/com/learning/yasminishop/common/entity/Notification.java
@@ -2,6 +2,7 @@ package com.learning.yasminishop.common.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Table(name = "t_notification")
@@ -10,6 +11,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Notification extends  AuditEntity<String>{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
